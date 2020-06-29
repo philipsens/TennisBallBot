bb8574e1b2b7501f1f4556e31b15bc4a3fc314da
Joey de Ruiter <contact@joeyderuiter.me>
2020-06-29 13:01:58 +0200

    Zumo controls are now time based
    The Zumo controls are now time based, the time is defined as the 3rd
    param in the command
    
    for example:
    move=-200=1000 will move the cart backwards with the speed of 200 for 1
    second.
    
    The serial buffer can be used as a queue, but dont abuse it since the
    buffer is not that big in size and might overflow quite easly

--- zumo/zumo.ino
+++ zumo/zumo.ino
@@ -6,31 +6,75 @@
 ZumoAPI::ZumoMotor motor;
 ZumoAPI::SerialManager serial = NULL;
 
-void processToken(const char* identifier, const char* value) {
+void processToken(const char* identifier, const char* value, const char* time) {
+    int32_t wait_time = atoi(time);
 
-    if (strcmp(identifier, "left") == 0) {
+    // The "move" command moves both the tracks in the same direction to go backwards or forwards
+    if (strcmp(identifier, "move") == 0) {
         int16_t speed = atoi(value);
 
         motor.leftTrack(speed);
+        motor.rightTrack(speed);
+        motor.update();
+
+        Serial.print("move=");
+        Serial.print(speed);
+        Serial.print("=");
+        Serial.println(wait_time);
+
+    } else
+    // The "left" command moves the left track backwards and the right track forwards so the cart turns to the left
+    if (strcmp(identifier, "left") == 0) {
+        int16_t speed = atoi(value);
+
+        motor.leftTrack(-speed);
+        motor.rightTrack(speed);
+        motor.update();
 
         Serial.print("left=");
-        Serial.println(speed);
+        Serial.print(speed);
+        Serial.print("=");
+        Serial.println(wait_time);
     } else
+    // The "right" command moves the left track forwards and the right track backwards so the cart turns to the right
     if (strcmp(identifier, "right") == 0) {
         int16_t speed = atoi(value);
 
-        motor.rightTrack(speed);
+        motor.leftTrack(speed);
+        motor.rightTrack(-speed);
+        motor.update();
 
         Serial.print("right=");
-        Serial.println(speed);
+        Serial.print(speed);
+        Serial.print("=");
+        Serial.println(wait_time);
     } else
+    // Goose goes "honks"
     if (strcmp(identifier, "honk") == 0) {
         ZumoBuzzer buzzer;
         buzzer.playNote(3 + 5 * 12, 250, 15);
 
         Serial.println("honk");
+    } else {
+        Serial.println("Not a valid action");
+        return;
     }
 
+    Serial.println("Starting action");
+
+    // Keep executing the command for n amount of time.
+    delay(wait_time);
+
+    Serial.println("Stopping action");
+
+    // Set the motors to idle after executing the action.
+    reset();
+}
+
+void reset() {
+    motor.leftTrack(0);
+    motor.rightTrack(0);
+    motor.update();
 }
 
 void processLine(char* line) {
@@ -38,8 +82,9 @@ void processLine(char* line) {
 
     const char* identifier  = strtok_r(line, delimiter, &line);
     const char* value       = strtok_r(line, delimiter, &line);
+    const char* time        = strtok_r(line, delimiter, &line);
 
-    processToken(identifier, value);
+    processToken(identifier, value, time);
 }
 
 void callback(char* buffer) {
