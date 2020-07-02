4f9a560dddc1fe0bf82e047222b8c2c52f96514f
Sergi Philipsen <philipsen.sergi@gmail.com>
2020-07-02 11:38:36 +0200

    Add pycache to gitignore

--- raspberry/tennis_ball_detection.py
+++ raspberry/tennis_ball_detection.py
@@ -1,31 +1,56 @@
 import time
 
+from simple_pid import PID
 from detector.ObjectDetector import ObjectDetector
 from zumo.zumo import Zumo
 
-zumo = Zumo("/dev/ttyACM0")
-zumo.start()
+MIDDLE_POINT = 0.5
+CLOSE_POINT = 0.98
+MAX_SPEED = 300
+USE_PID = False
 
-time.sleep(1)
+zumo = Zumo("/dev/ttyACM0")
+zumo.run('stop')
+
+direction_pid = PID(1, 0.1, 0.05, setpoint=MIDDLE_POINT)
+distance_pid = PID(1, 0.1, 0.05, setpoint=CLOSE_POINT)
+direction_pid.output_limits = (-0.5, 0.5)
+distance_pid.output_limits = (0, 1)
+
+
+def get_detection(detections):
+    detection = get_nearest_detection(detections)
+
+    if detection.width > CLOSE_POINT:
+        zumo.run('stop')
+
+        print("done")
+        return
+
+    if USE_PID:
+        direction_control = direction_pid(detection.position)
+        distance_control = distance_pid(detection.width)
+        print(("pid's: ", direction_control, distance_control))
+    else:
+        direction_control = detection.position - 0.5
+        distance_control = 1 - detection.width
+        print(("delta's: ", direction_control, distance_control))
+
+    speed = abs(max(distance_control * MAX_SPEED, 150))
+    speed_left = abs(MIDDLE_POINT - direction_control) * speed
+    speed_right = abs(MIDDLE_POINT + direction_control) * speed
+    print(("speeds's: ", speed, speed_left, speed_right))
+
+    zumo.run('left', speed_left)
+    zumo.run('right', speed_right)
 
 
 def get_nearest_detection(detections):
-    return max(detections, key=lambda detection: detection.width)
+    detection = max(detections, key=lambda d: d.width)
+    print(("detection: ", detection.class_id, detection.position, detection.width))
+    return detection
 
-
-def get_ball(detections):
-
-    ball = get_nearest_detection(detections)
-    if ball.width < 1:
-        if 0.4 < ball.position < 0.6:
-            zumo.add('move', 150, 100)
-            time.sleep(0.5)
-        elif ball.position <= 0.4:
-            zumo.add('left', 150, 50)
-        elif ball.position >= 0.4:
-            zumo.add('right', 150, 50)
-
-    time.sleep(0.25)
-
-
-ObjectDetector().start(get_ball)
+try:
+    ObjectDetector().start(get_detection)
+except KeyboardInterrupt:
+    zumo.run('stop')
